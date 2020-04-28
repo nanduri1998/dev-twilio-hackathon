04c400471dfbb997b4574190a761014cee2fc192
Srivastav Nanduri <srivastav.nanduri.98@gmail.com>
2020-04-28 17:18:41 +0530

    Changed Links to https

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -9,10 +9,10 @@ export class DashboardService {
   constructor(private http: HttpClient) { }
 
   getLocation() {
-    return this.http.get<any>('http://ip-api.com/json');
+    return this.http.get<any>('https://ipapi.co/json/');
   }
 
   getWeather(city) {
-    return this.http.get<any>('http://api.openweathermap.org/data/2.5/weather?appid=8598c1b0b4e91b68974101d97a10552b&q=' + city);
+    return this.http.get<any>('https://api.openweathermap.org/data/2.5/weather?appid=8598c1b0b4e91b68974101d97a10552b&q=' + city);
   }
 }
