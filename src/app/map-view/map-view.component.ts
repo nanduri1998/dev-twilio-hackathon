638f8890f7561585c445b82dc75842b214f8cb76
Srivastav Nanduri <srivastav.nanduri.98@gmail.com>
2020-04-30 20:22:17 +0530

    Added Changes for map view

--- src/app/map-view/map-view.component.ts
+++ src/app/map-view/map-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../services/dashboard.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-map-view',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-view.component.css']
 })
 export class MapViewComponent implements OnInit {
-
-  constructor() { }
+  location;
+  total;
+  totalDeaths;
+  totalDischarged;
+  notAvailable = '';
+  constructor(private dashboardService: DashboardService, private router: Router) { }
 
   ngOnInit() {
+    this.dashboardService.getdataSummary().subscribe(data => {
+      this.location = 'India';
+      this.total = data.data.summary.total;
+      this.totalDeaths = data.data.summary.deaths;
+      this.totalDischarged = data.data.summary.discharged;
+    });
+  }
+
+  getCases(state) {
+    if (state === 'India') {
+      this.notAvailable = 'Stats not available for your state';
+    } else {
+      this.notAvailable = '';
+      this.dashboardService.getdataSummary().subscribe(data => {
+        data.data.regional.forEach(stateindata => {
+          if (state === stateindata.loc) {
+            this.location = stateindata.loc;
+            this.total = stateindata.totalConfirmed;
+            this.totalDeaths = stateindata.deaths;
+            this.totalDischarged = stateindata.discharged;
+          }
+        });
+      });
+    }
+  }
+
+  back() {
+    this.router.navigate(['/dashboard']);
   }
 
 }
